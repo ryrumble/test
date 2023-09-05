0b16db15518851d7ab455a183eeb91b19c93f0e7
ryrumble <47289587+ryrumble@users.noreply.github.com>
2023-09-04 23:32:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
-#This is my first Flask app.
-from flask import Flask
-#create an object with the Flask module
-app = Flask(__name__)
-#tell Flask when to call the hello function
-@app.route("/")
-#show something in the browser
-def hello():
-    return "Hello World!";
+"""
+From this site: https://automatetheboringstuff.com/2e/chapter4/
+Say you have a list value like this:
 
-#another route is made with an 'its me'function
-@app.route("/ryan")
-def itsme():
-    return "here is page ryan"
+spam = ['apples', 'bananas', 'tofu', 'cats']
 
-#call the run function to run the app
-    #making a note.. app failed to run first tiem, so waitress was installed
-    #i commented out the line app.run(debug = True) and used the serve function
-    #and then it worked!
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-    #app.run(debug = True)
+Write a function that takes a list value as an argument and returns a
+string with all the items separated by a comma and a space, with and
+inserted before the last item. For example, passing the previous spam
+list to the function would return 'apples, bananas, tofu, and cats'.
+But your function should be able to work with any list value passed to it.
+Be sure to test the case where an empty list [] is passed to your function.
+"""
+def format_list(input_list):
+    if len(input_list) == 0:
+        return ""
+    elif len(input_list) == 1:
+        return input_list[0]
+    else:
+        formatted_items = ', '.join(input_list[:-1])
+        return f"{formatted_items}, and {input_list[-1]}"
+
+# Test cases
+spam = ['apples', 'bananas', 'tofu', 'cats']
+result = format_list(spam)
+print(result)  # Output: 'apples, bananas, tofu, and cats'
+
+empty_list = []
+result_empty = format_list(empty_list)
+print(result_empty)  # Output: ''
